67ea36d170489b125b5ebe9101755f584a797da7
dejanjevtic <dejan_d_jevtic@yahoo.com>
2021-10-22 10:14:46 +0200

    celobrojna promenljiva p
    opis

--- src/novprojekata/NovProjekatA.java
+++ src/novprojekata/NovProjekatA.java
@@ -15,7 +15,7 @@ public class NovProjekatA {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int p = 6;
     }
     
 }
